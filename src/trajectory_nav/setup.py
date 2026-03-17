d9a31dd4f599f7d878d44e1b09c4962653e53d65
Sunil Raj <sraj132000@gmail.com>
2026-03-17 14:47:25 +0530

    Initial Commit

---
+++ src/trajectory_nav/setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_packages, setup
+
+package_name = 'trajectory_nav'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/trajectory_nav.launch.py']),
+        ('share/' + package_name + '/config', [
+            'config/spline_smoother.yaml',
+            'config/trajectory_generator.yaml',
+            'config/pure_pursuit_controller.yaml',
+        ]),
+    ],
+    install_requires=['setuptools', 'numpy', 'scipy', 'matplotlib', 'tf-transformations'],
+    zip_safe=True,
+    maintainer='hppmp',
+    maintainer_email='sraj132000@gmail.com',
+    description='Trajectory navigation package with waypoint loading, spline smoothing, and pure pursuit control',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'waypoint_loader=trajectory_nav.waypoint_loader:main',
+            'spline_smoother=trajectory_nav.spline_smoother:main',
+            'trajectory_generator=trajectory_nav.trajectory_generator:main',
+            'pure_pursuit_controller=trajectory_nav.pure_pursuit_controller:main',
+            'trajectory_visualizer=trajectory_nav.trajectory_visualizer:main',
+        ],
+    },
+)
